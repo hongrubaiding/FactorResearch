574a259acb7848a5481402373dc601c23c8e34f6
zouhao <1084848158@qq.com>
2021-07-28 09:47:51 +0800

    FactorMain commit

---
+++ FactorMain.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 22 09:36:53 2021
+
+@author: zouhao
+"""
+
+
+import mylog as mylog_demo
+
+class FactorMain:
+    def __init__(self,):
+        self.logger = mylog_demo.set_log("因子研究日志")
+        self.logger.info("开始了")
+    
+    
+    
+    def start_main(self,):
+        A=0
+        B=8
+        pass
+    
+    
+if __name__=="__main__":
+    FactorMainDemo = FactorMain()
+    FactorMainDemo.start_main()
